0f71bdb4a33bdd045a2a41bb996232040bea96a6
cvangundy <van-gundy.15@osu.edu>
2015-03-08 16:30:51 -0400

    Added SplashScreen functionality

---
+++ app/src/main/java/mobileapps/cse5236/cubr/SplashScreen.java
@@ -0,0 +1,37 @@
+package mobileapps.cse5236.cubr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+    private Thread splashThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final SplashScreen splashScreen = this;
+
+        splashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        wait(5000);
+                    }
+                }
+                catch(InterruptedException ex){
+                    System.exit(-1);
+                }
+
+                finish();
+
+                Intent intent = new Intent();
+                intent.setClass(splashScreen, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        splashThread.start();
+    }
+}
